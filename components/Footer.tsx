708bdcbb9297d86fae05c8f022933f6c41847430
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2025-07-29 23:40:10 -0300

    fix: dynamic year on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -151,8 +151,8 @@ export default function Footer() {
         <div className="border-t border-theme mt-8 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-subtext text-secondary">
-              © 2024 Academia Panobianco Jardim Satélite. Todos os direitos
-              reservados.
+              © {new Date().getFullYear()} Academia Panobianco Jardim Satélite.
+              Todos os direitos reservados.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               <Link
